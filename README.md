66a6fe17890d55a07776195b8bd7537c8a5c2f7c
Slavus54 <x.mirosfromsibir@gmail.com>
2024-05-16 14:47:37 +0700

    Added vercel config

---
+++ README.md
@@ -0,0 +1,29 @@
+## Towns API  
+
+### Description  
+
+Open REST API about european towns and context of them: old cathedrals and fortresses.  
+
+You can find any of 475 towns with rapidly search of *centum.js* library.    
+
+### Technologies  
+
+Server was built on *ExpressJS* with middlewares.    
+
+### URLs  
+
+*https://towns-api.onrender.com* - base URL    
+
+*https://towns-api.onrender.com/towns* - list of towns    
+
+*https://towns-api.onrender.com/town/:title* - sliced list of towns   
+
+*https://towns-api.onrender.com/towns-fragment/:start&:end* - find of town by title     
+
+*https://towns-api.onrender.com/timezones* - list of european towns's timezones   
+
+*https://towns-api.onrender.com/capitals* - list of european capitals with additional information    
+
+*https://towns-api.onrender.com/cathedrals* - oldest european cathedrals of many towns with interesting information
+
+*https://towns-api.onrender.com/fortresses* - fortresses with additional information 
---
+++ index.js
@@ -0,0 +1,82 @@
+require('dotenv').config()
+
+// files
+
+const towns = require('./api/towns.json')
+const timezones = require('./api/timezones.json')
+const capitals = require('./api/capitals.json')
+const cathedrals = require('./api/cathedrals.json')
+const fortresses = require('./api/fortresses.json')
+
+// libs
+
+const app = require('express')()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const Centum = require('centum.js')
+
+const centum = new Centum()
+
+// options
+
+const PORT = process.env.PORT
+const length = towns.length  
+
+// middlewares
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.get('/', async (req, res) => {
+    res.sendStatus(200)
+})
+
+app.get('/towns', async (req, res) => {
+    res.send(towns)
+})
+
+app.get('/towns-fragment/:start&:end', async (req, res) => {
+    let {start, end} = req.params
+    
+    start = borderCheck(start)
+    end = borderCheck(end)
+
+    let result = towns.slice(start, end)
+
+    res.send(result)
+})
+
+app.get('/town/:title', async (req, res) => {
+    let {title} = req.params
+
+    let town = towns.find(el => centum.search(el.title, title, process.env.SEARCH_PERCENT, true))
+
+    if (town !== undefined) {
+        res.send(town)
+    } else {
+        res.sendStatus(404)
+    }
+})
+
+app.get('/timezones', async (req, res) => {
+    res.send(timezones)
+})
+
+app.get('/capitals', async (req, res) => {
+    res.send(capitals)
+})
+
+app.get('/cathedrals', async (req, res) => {
+    res.send(cathedrals)
+})
+
+app.get('/fortresses', async (req, res) => {
+    res.send(fortresses)
+})
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+
+function borderCheck(num) {
+    return num < 0 || num > length ? 0 : num
+}
